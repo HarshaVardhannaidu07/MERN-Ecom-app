3f43708f0c37a172f7b98747f1a389fd258cb817
HarshaVardhannaidu07 <naiduhv07@gmail.com>
2024-02-06 13:31:48 +0530

    Basic Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Outlet } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
     <>
-    <h1>Welcome</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet/>
+        </Container>
+      </main>
+      <Footer/>
     </>
   );
-}
+};
 
 export default App;
