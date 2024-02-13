eb20a32729b2bde489803c6301097502670d180d
HarshaVardhannaidu07 <naiduhv07@gmail.com>
2024-02-13 19:42:14 +0530

    Baackend/ frontend authentication completed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,35 @@
-import { Badge,Navbar,Nav,Container}from 'react-bootstrap';
+import {useNavigate} from 'react-router-dom';
+import { Badge,Navbar,Nav,Container,NavDropdown}from 'react-bootstrap';
 import {FaShoppingCart , FaUser} from 'react-icons/fa';
 import { LinkContainer } from 'react-router-bootstrap';
 import logo from "../assets/logo.png";
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import { useLogoutMutation } from '../slices/usersApiSlice';
+import { logout } from '../slices/authSlice';
 
 const Header = () => {
 
 
     const { cartItems} = useSelector((state) => state.cart);
-    console.log(cartItems);
+
+    const {userInfo} = useSelector((state) => state.auth);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+   
+    const [logoutApiCall] = useLogoutMutation();
+    
+    const logoutHandler= async () =>{
+        try{
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate('/login');
+        }
+        catch(err){
+            console.log(err);
+        }
+    };
+
 
   return (
       <header>
@@ -26,11 +47,29 @@ const Header = () => {
                     <LinkContainer to="/cart"><Nav.Link href="/cart"><FaShoppingCart/>Cart{
                         cartItems.length > 0 && (
                             <Badge pill bg='success' style={{marginLeft: '5px'}}> {cartItems.reduce((a,c) => a + c.qty, 0)} </Badge> // a represents accumulator and c represent current item  and  0 is passed as default accumulator
-                        )
-
-                    }</Nav.Link></LinkContainer>
-                    <LinkContainer to="/Sign In"><Nav.Link href="/Sign In"><FaUser/>Sign In</Nav.Link></LinkContainer>
-                    </Nav>
+                        )}    
+                    </Nav.Link></LinkContainer>
+                    {userInfo ? (
+                   <>
+                          <NavDropdown title={userInfo.name} id='username'>
+                          <LinkContainer to='/profile'>
+                          <NavDropdown.Item>Profile</NavDropdown.Item>
+                          </LinkContainer>
+                          <NavDropdown.Item onClick={logoutHandler}>
+                          Logout
+                          </NavDropdown.Item>
+                          </NavDropdown>
+                   </>
+                    ) : (
+                      <LinkContainer to='/login'>
+                      <Nav.Link>
+                      <FaUser /> Sign In
+                      </Nav.Link>
+                      </LinkContainer>
+                   )}
+        
+        
+                  </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,9 @@ import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from 
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import {toast} from 'react-toastify'
+
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -16,6 +19,9 @@ const router = createBrowserRouter(createRoutesFromElements(
   <Route index={true} path='/' element={<HomeScreen/>}/>
   <Route path='/product/:id' element={<ProductScreen/>}/>
   <Route path='/cart' element={<CartScreen/>}/>
+  <Route path='/login' element={<LoginScreen/>}/>
+
+  
 
   
    
